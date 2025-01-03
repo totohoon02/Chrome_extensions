2a69292dfaba02581200d5b019a8be0dc97446d1
SangHoon Kang <53715807+totohoon02@users.noreply.github.com>
2025-01-03 09:10:42 +0900

    Update popup.js

--- AutoDocs/scripts/popup.js
+++ AutoDocs/scripts/popup.js
@@ -1,26 +1,58 @@
 const input_file = document.querySelector("#javafile");
-const test = document.querySelector("#test");
+const input_text = document.querySelector("#javatext");
+const result = document.querySelector("#result");
 const btn_copy = document.querySelector("#btn_copy");
 const btn_download = document.querySelector("#btn_download");
+const select = document.querySelector("#doctype");
 
 let template = "";
 let className = "";
+let suffix = "Docs";
+let doctype = "swagger";
+let rtn_text = [];
+
+
+select.addEventListener('change', (event) => {
+    const value = event.target.value;
+    if(value == "swagger"){
+        suffix = "Docs";
+        doctype = "swagger";
+    }
+    else if(value == "service"){
+        suffix = "";
+        doctype = "service"
+    }
+    else if(value == "repository"){
+        suffix = "";
+        doctype = "service";
+    }
+});
 
 btn_copy.addEventListener("click", function(){
-    navigator.clipboard.writeText(template);
+    if(className == "" || template == "") return;
     alert("카피!");
+    navigator.clipboard.writeText(template);
 });
 
 btn_download.addEventListener("click", function(){
+    if(className == "" || template == "") return;
     alert("다운로드!");
     const blob = new Blob([template], { type: 'text/plain' });
     const link = document.createElement('a');
     link.href = URL.createObjectURL(blob); 
-    link.download = `${className}Docs.java`;
+    link.download = `${className}${suffix}.java`;
     link.click();
 });
 
+input_text.addEventListener("input", function(ev) {
+    input_file.value = "";
+    let code = ev.target.value;
+    generate(code);
+    result.value = template;
+});
+
 input_file.addEventListener("change", function (ev) {
+    input_text.value = "";
     const file = ev.target.files[0];
 
     if(!file) return;
@@ -28,56 +60,77 @@ input_file.addEventListener("change", function (ev) {
     const reader = new FileReader();
         
     reader.onload = function (ev) {
-        btn_copy.style.display = "block";
-        btn_download.style.display = "block";
         if (ev.target.result) {
             let code = ev.target.result;
-            let lines = code.split("\n");
-            let rtn_text = [];
-            let temp_text;
-            
-            // read file
-            for(let i=0; i< lines.length; i++){
-                let line = lines[i].trim()
-
-                // class name
-                if(line.startsWith("public class")){
-                    className = line.replace("public class ", "").trim().split(" ")[0];
-                }
-
-                // method
-                if(line.startsWith("public") && !line.startsWith("public class")){
-                    temp_text = "\t" + line + "\n";
-                    
-                    // multiline parameter
-                    while(true){
-                        if(lines[i].endsWith("{")){
-                            break;
-                        }
-                        i++;
-                        temp_text += " " + lines[i].trim();
-                    }
-                    temp_text = temp_text.replace("public", "");
-                    temp_text = temp_text.replace("{", "");
-                    temp_text = temp_text.trim() + ";"
-                    rtn_text.push(temp_text);
-                    
-                }
-            }
-            
-            // write file
-            template = `@Tag(name = "${className}", description = "")\n`;
-            template += `public interface ${className}Docs {\n`;
-            
-            for(let i=0; i<rtn_text.length; i++){
-                template += '\t' + `@Operation(summary = "", description = "")\n`;
-                template += '\t' + rtn_text[i] + '\n';
-                template += '\n';
-            }
-            
-            template += '}';
+            generate(code);
+            result.value = template;
         }
     }
     
     reader.readAsText(file);
 });
+
+
+function generate(code){
+    let lines = code.split("\n");
+    let temp_text = "";
+    rtn_text = [];
+
+    // read file
+    for(let i=0; i< lines.length; i++){
+        let line = lines[i].trim()
+        
+        if(!line.startsWith("public")){
+            continue;
+        }
+
+        if(line.startsWith("public class")){
+            line = line.replace("public class", "");
+            line = line.replace(" {", "");
+            className = line.trim()
+            continue
+        }
+
+        while(true){
+            temp_text += lines[i].trim();
+            if(temp_text.endsWith("{")) break;
+            i++;
+        }
+        temp_text = temp_text.replace(",", ", ");
+        temp_text = temp_text.replace("public", "")
+        temp_text = temp_text.replace("{", "");
+        temp_text = temp_text.trim() + ";";
+
+        rtn_text.push(temp_text);
+        temp_text = "";
+    }
+
+    if(doctype == "swagger"){
+        swagger_template();
+    }
+    else {
+        interface_template();
+    }
+}
+
+function swagger_template(){
+    template = `@Tag(name = "${className}", description = "")\n`;
+    template += `public interface ${className}${suffix} {\n`;
+    
+    for(let i=0; i<rtn_text.length; i++){
+        template += '\t' + `@Operation(summary = "", description = "")\n`;
+        template += '\t' + rtn_text[i] + '\n';
+        template += '\n';
+    }
+    
+    template += '}';
+}
+
+function interface_template(){
+    template = `public interface ${className}${suffix} {\n`;
+    for(let i=0; i<rtn_text.length; i++){
+        template += '\t' + rtn_text[i] + '\n';
+        template += '\n';
+    }
+    template += '}';
+}
